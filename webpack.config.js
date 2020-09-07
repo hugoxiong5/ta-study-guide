5fee1d13c74bbaa64f0b9c47623615e7c13a2c03
Hugo Xiong <hugoxiong@gmail.com>
2020-09-07 01:42:27 -0700

    config webpack, add index.html with bootstrap

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 var path = require("path");
-var SRC_DIR = path.join(__dirname, "/react-client/src");
-var DIST_DIR = path.join(__dirname, "/react-client/dist");
+var SRC_DIR = path.join(__dirname, "/client/src");
+var DIST_DIR = path.join(__dirname, "/client/dist");
 
 module.exports = {
   entry: `${SRC_DIR}/index.jsx`,
@@ -9,15 +9,51 @@ module.exports = {
     path: DIST_DIR,
   },
   module: {
-    loaders: [
+    rules: [
       {
-        test: /\.jsx?/,
-        include: SRC_DIR,
-        loader: "babel-loader",
-        query: {
-          presets: ["@babel/preset-react", "@babel/preset-env"],
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
         },
       },
     ],
   },
 };
+
+// module.exports = {
+//   entry: `${SRC_DIR}/index.jsx`,
+//   output: {
+//     filename: "bundle.js",
+//     path: DIST_DIR,
+//   },
+//   module: {
+//     loaders: [
+//       {
+//         test: /\.jsx?/,
+//         include: SRC_DIR,
+//         loader: "babel-loader",
+//         query: {
+//           presets: ["@babel/preset-env", "@babel/preset-react"],
+//         },
+//       },
+//     ],
+//   },
+// };
+
+// module.exports = {
+//   module: {
+//     rules: [
+//       {
+//         test: /\.(js|jsx)$/,
+//         exclude: /node_modules/,
+//         use: {
+//           loader: "babel-loader"
+//         }
+//       }
+//     ]
+//   }
+// };
