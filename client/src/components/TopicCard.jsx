eb666f5728aebb7df1befe9f1ff30ce51d6a219a
Hugo Xiong <hugoxiong@gmail.com>
2020-09-07 16:58:07 -0700

    add babel plugin to use "public class fields"

--- client/src/components/TopicCard.jsx
+++ client/src/components/TopicCard.jsx
@@ -1,46 +1,87 @@
 import React from "react";
 import { Card, Accordion, Button } from "react-bootstrap";
 
-const TopicCard = ({ topic }) => {
-  return (
-    <Accordion className="m-3">
-      <Card>
-        <Card.Header>
-          <Accordion.Toggle as={Button} variant="success" eventKey={topic.id}>
-            {topic.title}
-          </Accordion.Toggle>
-          <div className="ranking">
-            <div>
-              <input type="radio" name={topic.id} value="3" />
-              <label>easy</label>
+class TopicCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedRank: 2,
+    };
+    // this.handleOptionChange = this.handleOptionChange.bind(this);
+  }
+
+  handleRankChange = (event) => {
+    this.setState({
+      selectedOption: +event.target.value,
+    });
+  };
+
+  render() {
+    const topic = this.props.topic;
+    return (
+      <Accordion className="m-3">
+        <Card>
+          <Card.Header>
+            <Accordion.Toggle as={Button} variant="success" eventKey={topic.id}>
+              {topic.title}
+            </Accordion.Toggle>
+            <div className="ranking">
+              <div>
+                <input
+                  type="radio"
+                  name={topic.id}
+                  value="3"
+                  checked={this.state.selectedRank === 3}
+                  onChange={this.handleRankChange}
+                />
+                <label>easy</label>
+              </div>
+              <div>
+                <input
+                  type="radio"
+                  name={topic.id}
+                  value="2"
+                  checked={this.state.selectedRank === 2}
+                  onChange={this.handleRankChange}
+                />
+                <label>got it</label>
+              </div>
+              <div>
+                <input
+                  type="radio"
+                  name={topic.id}
+                  value="1"
+                  checked={this.state.selectedRank === 1}
+                  onChange={this.handleRankChange}
+                />
+                <label>so-so</label>
+              </div>
+              <div>
+                <input
+                  type="radio"
+                  name={topic.id}
+                  value="0"
+                  checked={this.state.selectedRank === 0}
+                  onChange={this.handleRankChange}
+                />
+                <label>hard</label>
+              </div>
             </div>
-            <div>
-              <input type="radio" name={topic.id} value="2" />
-              <label>got it</label>
-            </div>
-            <div>
-              <input type="radio" name={topic.id} value="1" />
-              <label>so-so</label>
-            </div>
-            <div>
-              <input type="radio" name={topic.id} value="0" />
-              <label>hard</label>
-            </div>
-          </div>
-        </Card.Header>
-        <Accordion.Collapse eventKey={topic.id}>
-          <Card.Body>
-            <Card.Text>{topic.text}</Card.Text>
-            <Card.Link href="#">Card Link</Card.Link>
-            {/* <Card.Link href="#">Another Link</Card.Link>
+          </Card.Header>
+          <Accordion.Collapse eventKey={topic.id}>
+            <Card.Body>
+              <Card.Text>{topic.text}</Card.Text>
+              <Card.Link href="#">Card Link</Card.Link>
+              {/* <Card.Link href="#">Another Link</Card.Link>
             <Card.Link href="#">Another Link</Card.Link>
             <Card.Link href="#">Another Link</Card.Link> */}
-          </Card.Body>
-        </Accordion.Collapse>
-      </Card>
-    </Accordion>
-  );
-};
+            </Card.Body>
+          </Accordion.Collapse>
+        </Card>
+      </Accordion>
+    );
+  }
+}
 
 function alertClicked() {
   // alert("You clicked a ListGroupItem");
--- webpack.config.js
+++ webpack.config.js
@@ -15,52 +15,12 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env", "@babel/preset-react"],
-          },
         },
       },
       {
         test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
-      }
+        use: ["style-loader", "css-loader"],
+      },
     ],
   },
 };
-
-// module.exports = {
-//   entry: `${SRC_DIR}/index.jsx`,
-//   output: {
-//     filename: "bundle.js",
-//     path: DIST_DIR,
-//   },
-//   module: {
-//     loaders: [
-//       {
-//         test: /\.jsx?/,
-//         include: SRC_DIR,
-//         loader: "babel-loader",
-//         query: {
-//           presets: ["@babel/preset-env", "@babel/preset-react"],
-//         },
-//       },
-//     ],
-//   },
-// };
-
-// module.exports = {
-//   module: {
-//     rules: [
-//       {
-//         test: /\.(js|jsx)$/,
-//         exclude: /node_modules/,
-//         use: {
-//           loader: "babel-loader"
-//         }
-//       }
-//     ]
-//   }
-// };
