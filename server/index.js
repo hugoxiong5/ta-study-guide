e4f9e7cd98ad1dcae3763a97ff906e0ca5931c1f
Hugo Xiong <hugoxiong@gmail.com>
2020-09-07 01:46:56 -0700

    add express skeleton

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "../client/dist")));
+
+let port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server listening on ${port}`);
+});
