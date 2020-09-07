1a1b0af50ab473aa900d37ea48d26092ea63029c
Hugo Xiong <hugoxiong@gmail.com>
2020-09-07 12:41:18 -0700

    add stuff

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,8 @@
 import React from "react";
+import { Button } from "react-bootstrap";
+
 import TopicList from "./components/TopicList.jsx";
-import { Modal, Button } from "react-bootstrap";
+import AddTopicModal from "./components/AddTopicModal.jsx";
 
 class App extends React.Component {
   constructor(props) {
@@ -42,44 +44,16 @@ class App extends React.Component {
         <h2>Topics</h2>
         <TopicList topics={this.state.topics} />
         <Button variant="primary" onClick={() => this.setModalShow(true)}>
-          Launch vertically centered modal
+          Add Topic
         </Button>
 
-        <MyVerticallyCenteredModal
+        <AddTopicModal
           show={this.state.modalShow}
           onHide={() => this.setModalShow(false)}
         />
       </>
     );
   }
-}
-
-function MyVerticallyCenteredModal(props) {
-  return (
-    <Modal
-      {...props}
-      size="lg"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered
-    >
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          Modal heading
-        </Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <h4>Centered Modal</h4>
-        <p>
-          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-          consectetur ac, vestibulum at eros.
-        </p>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button onClick={props.onHide}>Close</Button>
-      </Modal.Footer>
-    </Modal>
-  );
 }
 
 export default App;
