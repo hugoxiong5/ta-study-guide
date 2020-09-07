42ed63a319d459f0f7f2613283ca867ac8057725
Hugo Xiong <hugoxiong@gmail.com>
2020-09-07 02:10:00 -0700

    add topic component skeleton

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,22 @@
 import React from "react";
+import TopicList from "./components/TopicList.jsx";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      topics: [],
+    };
   }
 
   render() {
-    return <h1>Study Guide</h1>;
+    return (
+      <>
+        <h1>Study Guide</h1>
+        <h2>Topics</h2>
+        <TopicList topics={this.state.topics} />
+      </>
+    );
   }
 }
 
---
+++ client/src/components/TopicList.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import TopicItem from "./TopicItem.jsx";
+
+const TopicList = ({ topics }) => {
+  return { topics };
+};
+
+export default TopicList;
