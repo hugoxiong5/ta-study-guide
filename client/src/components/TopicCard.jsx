061a4b55a5f0d880b0365952ca934b97a207cfd6
Hugo Xiong <hugoxiong@gmail.com>
2020-09-07 16:28:10 -0700

    add ranking to to do list

--- client/src/components/TopicCard.jsx
+++ client/src/components/TopicCard.jsx
@@ -3,55 +3,42 @@ import { Card, Accordion, Button } from "react-bootstrap";
 
 const TopicCard = ({ topic }) => {
   return (
-    <>
-      <Accordion className="m-3">
-        <Card>
-          <Card.Header>
-            <Accordion.Toggle as={Button} variant="success" eventKey={topic.id}>
-              {topic.title}
-            </Accordion.Toggle>
+    <Accordion className="m-3">
+      <Card>
+        <Card.Header>
+          <Accordion.Toggle as={Button} variant="success" eventKey={topic.id}>
+            {topic.title}
+          </Accordion.Toggle>
+          <div className="ranking">
             <div>
-              <input
-                type="radio"
-                id="contactChoice1"
-                name="contact"
-                value="email"
-              />
-              <label htmlFor="contactChoice1">Email</label>
-
-              <input
-                type="radio"
-                id="contactChoice2"
-                name="contact"
-                value="phone"
-              />
-              <label htmlFor="contactChoice2">Phone</label>
-
-              <input
-                type="radio"
-                id="contactChoice3"
-                name="contact"
-                value="mail"
-              />
-              <label htmlFor="contactChoice3">Mail</label>
+              <input type="radio" name={topic.id} value="3" />
+              <label>easy</label>
             </div>
-          </Card.Header>
-          <Accordion.Collapse eventKey={topic.id}>
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Subtitle className="mb-2 text-muted"></Card.Subtitle>
-              <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-                {topic.text}
-              </Card.Text>
-              <Card.Link href="#">Card Link</Card.Link>
-              <Card.Link href="#">Another Link</Card.Link>
-            </Card.Body>
-          </Accordion.Collapse>
-        </Card>
-      </Accordion>
-    </>
+            <div>
+              <input type="radio" name={topic.id} value="2" />
+              <label>got it</label>
+            </div>
+            <div>
+              <input type="radio" name={topic.id} value="1" />
+              <label>so-so</label>
+            </div>
+            <div>
+              <input type="radio" name={topic.id} value="0" />
+              <label>hard</label>
+            </div>
+          </div>
+        </Card.Header>
+        <Accordion.Collapse eventKey={topic.id}>
+          <Card.Body>
+            <Card.Text>{topic.text}</Card.Text>
+            <Card.Link href="#">Card Link</Card.Link>
+            {/* <Card.Link href="#">Another Link</Card.Link>
+            <Card.Link href="#">Another Link</Card.Link>
+            <Card.Link href="#">Another Link</Card.Link> */}
+          </Card.Body>
+        </Accordion.Collapse>
+      </Card>
+    </Accordion>
   );
 };
 
