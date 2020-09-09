4445ef91fa2e785b89a686045b8815ee9492964a
Hugo Xiong <hugoxiong@gmail.com>
2020-09-09 14:48:30 -0700

    add more conditional logic to listItem

--- client/src/components/TopicCard.jsx
+++ client/src/components/TopicCard.jsx
@@ -116,7 +116,7 @@ class TopicCard extends React.Component {
                     onClick={() => this.props.setEditModalShow(true, topic)}
                   ></i>
                 </div>
-                {listItems.length > 0 ? (
+                {listItems && listItems.length > 1 ? (
                   <>
                     <hr />
                     <ListGroup as="ul" variant="flush">
